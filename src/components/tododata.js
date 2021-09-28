9544d701c03e3d87cfdd8cbadd6a5f3f33cc9162
Hedieh Moshtaghi <moshtaghi.he@gmail.com>
2021-09-28 09:32:20 +0330

    change checkbox!

--- src/components/tododata.js
+++ src/components/tododata.js
@@ -5,25 +5,25 @@ const axios = require('axios').default;
 function Tododata(){
 
     const [todo,setTodo] = useState([])
-    const [state, setState] = useState("all");
+    const [status, setStatus] = useState("all");
     const [filteredTodos, setFilteredTodos] = useState([]);
+    // const[check,setCheck] = useState()
+    // const[checkId,setCheckId]=useState()
 
-
-    //testing
     useEffect(() =>{
             axios.get('https://jsonplaceholder.typicode.com/todos')
             .then((res) => {
                 let datas = res.data.map( value => {
-                    return {title: value.title, completed: value.completed }
+                    return {id: value.id,title: value.title, completed: value.completed }
                     });
                     setTodo(datas)
-                    setFilteredTodos(datas)
+                    setStatus("all")
                 }
             )
     },[])
 
     useEffect(() => {
-        switch(state){
+        switch(status){
                 case "all":
                     setFilteredTodos(todo);
                     break;
@@ -33,12 +33,23 @@ function Tododata(){
                 case "uncompleted":
                     setFilteredTodos(todo.filter(td => !td.completed));
         }
-    }, [state])
+    }, [status,todo])
  
-    // const handleCompleted = (value) => {
-    //    return value.completed ? "checked" : null 
-    // }
+//    const handleCompleted = (value) => {
+//         return !value 
+//     }
+    // useEffect(()=> {
+    //     // const com={completed: check}
+    //     axios.patch(`https://jsonplaceholder.typicode.com/todos/${checkId}`,{completed: check})
+    //     .then(response => console.log(response.data));
+    // },[check])
 
+    const changeCompleteState = (value,id) => {
+        setTodo(todo.map(todo => {
+            if(todo.id===id) return{...todo,completed:value};
+            return todo;
+        }))
+    }
     return(
         <div>
             <p className="header">To Do :</p>
@@ -49,9 +60,9 @@ function Tododata(){
                                 Dropdown
                             </button>
                             <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <li><button className="dropdown-item" type="button" onClick={() => setState("all")}>All</button></li>
-                                <li><button className="dropdown-item" type="button" onClick={() => setState("completed")}>Completed</button></li>
-                                <li><button className="dropdown-item" type="button" onClick={() => setState("uncompleted")}>UnCompleted</button></li>
+                                <li><button className="dropdown-item" type="button" onClick={() => setStatus("all")}>All</button></li>
+                                <li><button className="dropdown-item" type="button" onClick={() =>  setStatus("completed")}>Completed</button></li>
+                                <li><button className="dropdown-item" type="button" onClick={() =>  setStatus("uncompleted")}>UnCompleted</button></li>
                             </ul>
                         </div>
                     </div>
@@ -59,7 +70,10 @@ function Tododata(){
                 <div className="list-group">
                     {filteredTodos.map((value,index) => <div key={index}>                
                         <label className="list-group-item" >
-                        <input className="form-check-input me-1" type="checkbox" value="" checked={(value.completed) ? "checked" : ""}/>
+                        <input className="form-check-input me-1" type="checkbox" value="" checked={(value.completed) ? "checked" : ""} 
+                        onChange={() =>{                            
+                            changeCompleteState(!value.completed, value.id)
+                        }} />
                             {value.title}
                         </label>
                          <br/></div>)}
